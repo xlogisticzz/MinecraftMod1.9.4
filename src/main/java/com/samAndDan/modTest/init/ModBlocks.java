6f279e16160a897aa68af88a7b545ad0ef096031
JuiceBox <xlogisticzzlp@gmail.com>
2016-06-07 18:56:02 +0100

    Fixed Everything

--- src/main/java/com/samAndDan/modTest/init/ModBlocks.java
+++ src/main/java/com/samAndDan/modTest/init/ModBlocks.java
@@ -3,6 +3,7 @@ package com.samAndDan.modTest.init;
 import com.samAndDan.modTest.block.BlockModOre;
 import com.samAndDan.modTest.block.ModBlock;
 import com.samAndDan.modTest.lib.Strings;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
 
@@ -11,9 +12,7 @@ public class ModBlocks {
 
     public static void createBlocks() {
         rubyOre = new BlockModOre(Strings.RUBYORE, 3F, 5F);
-
-
-        //  GameRegistry.register(rubyOre, new ResourceLocation(Strings.MODID, Strings.RUBYORE));
+        GameRegistry.registerBlock(rubyOre, Strings.RUBYORE);
 
     }
 
--- src/main/java/com/samAndDan/modTest/modCreativeTab.java
+++ src/main/java/com/samAndDan/modTest/modCreativeTab.java
@@ -1,21 +1,15 @@
 package com.samAndDan.modTest;
 
+import com.samAndDan.modTest.init.ModItems;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-
-/**
- * Created by Juicebox on 25/06/2015.
- * For LearningModding at https://github.com/xlogisticzz/LearningModding
- * Licensed under GNU GENERAL PUBLIC LICENSE
- */
 
 public class ModCreativeTab {
 
     public static CreativeTabs tabMod = new CreativeTabs("tabMod") {
         @Override
         public Item getTabIconItem() {
-            return Items.APPLE;
+            return ModItems.ruby;
         }
     };
 }
