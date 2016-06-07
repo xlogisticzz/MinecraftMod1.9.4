4b14237901e165a9ce4ba0724bfec8ca4f9cdd99
JuiceBox <xlogisticzzlp@gmail.com>
2016-06-07 16:17:54 +0100

    Test

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,15 +6,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
+public class ExampleMod {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
     }
 }
